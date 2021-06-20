d63caa50b2e29d4e0382749551f48e8e6ea3c046
aligg <ali.glenesk@gmail.com>
2021-06-20 13:48:57 -0700

    there we go

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,14 @@ import "../App.css";
 import BuilderContainer from "./BuilderContainer";
 
 const App = () => {
-  return (
-    <div className="app">
-        <header className="app-header">
-        <p>FHIR Questionnaire Builder</p>
-        </header>
-        <BuilderContainer />
-    </div>
-  )
+    return (
+        <div className="app">
+            <header className="app-header">
+                <p>FHIR Questionnaire Builder</p>
+            </header>
+            <BuilderContainer />
+        </div>
+    );
 };
 
 export default App;
